271a30a87896ecd39cce857ddb4f22d95fa4e6f0
Dave <dfnum2@gmail.com>
2022-01-30 22:59:00 -0600

    Added link for source

--- thruster.ino
+++ thruster.ino
@@ -20,6 +20,9 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
 //
+// Source code at:
+//
+// https://github.com/dfnr2/asteroids-lunar-multigame-thrust-a2d
 
 
 // Wiring Information:
