1e68233d6195ffd86cfa48e09ba64a1b4f7ba851
David Fenyes <dave@greenumelec.com>
2022-03-15 14:39:46 -0500

    Finished wiring comments

--- thruster/thruster.ino
+++ thruster/thruster.ino
@@ -26,27 +26,29 @@
 
 
 // Wiring Information:
-// Chip: ATMega328P, Arduino mini or Arduino Nano
-//
+// Chip: ATMega328P, 32-pin TQFP
+// Note: Pin numbers indicate the Atmega328P pin number, not the arduino board
+//       pin.
+
 // PIN     NAME   HEADER PIN   FUNCTION
-// 3       RST    1            Power connection to the 10-pin header.  RST and 5V are connected
+// 29      RST    1            Power connection to the 10-pin header.  RST and 5V are connected
 //                             through the potentiometer cable, as shown in the simulation.
 //
-// 4       GND    2            GND connection on the 10-pin header.
+//         GND    2            GND connection on the 10-pin header.
 //
 //                             Note: The header directly maps to the pokey pins, which swaps
 //                             each pair of bits, so that each pair of bits in the header is
 //                             swapped.  So outputs D2-D9 don't correspond directly to bits 0-7
 //                             of the POKEY.  THe odd and even bits are swapped in software.
 //
-// 5       D2     3            POKEY D1
-// 6       D3     4            POKEY D0
-// 7       D4     5            POKEY D3
-// 8       D5     6            POKEY D2
-// 9       D6     7            POKEY D5
-// 10      D7     8            POKEY D4
-// 11      D8     9            POKEY D7
-// 12      D9     10           POKEY D6
+// 32      D2     3            POKEY D1
+// 1       D3     4            POKEY D0
+// 2       D4     5            POKEY D3
+// 9       D5     6            POKEY D2
+// 10      D6     7            POKEY D5
+// 11      D7     8            POKEY D4
+// 12      B0     9            POKEY D7
+// 13      B1     10           POKEY D6
 //
 //         On the arduino nano, the A7 pin is adjacent to the +5 pin. On the
 //         mini pro, the A3 pin is adjacent to the +5 pin. Using A7 on the nano
@@ -54,20 +56,85 @@
 //         potentiometer, with the RESET and +5 pins both soldered to the high
 //         side lug of the potentiometer.
 
-// 22      A7                  Analog in (nano) Connected to the wiper on the
-//                             thruster potentiometer
-//
-// 26      A3                  Analog in.(mini pro) Connected to the wiper on
+// **************** The following pins are used for the Nano ************************
+// 22      ADC7                Analog in.(mini pro) Connected to the wiper on
 //                             the thruster potentiometer
 //
-// 27      5V                  Power.  Connected to pin 28 (RST) (which is connected to pin 1 (+5)
-//                             on the 10-pin header) to power the board.  Also provides 5V to the
-//                             high side of the thruster potentiometer.
+// 28      C5                  EEPROM_CLEAR2: (input)
+// 27      C4                  EEPROM_CLEAR1: (output)
+//                             EEPROM is cleared if these lines are shorted at startup
+// ***********************************************************************************
 //
-// 28      RST                 This is connected to pin 27 (5V) to power the device.
+// **************** The following pins are used for the Mini Pro *********************
+// 26      C3                  Analog in.(mini pro) Connected to the wiper on
+//                             the thruster potentiometer
 //
-// 29      GND                 Provides 0V to the potentiometer
-
+// 24      C1                  EEPROM_CLEAR2: (input)
+// 23      C0                  EEPROM_CLEAR1: (output)
+//                             EEPROM is cleared if these lines are shorted at startup
+// ***********************************************************************************
+//
+//
+//
+// Arduino Nano pinout:
+//
+//                                       +-----+
+//                          +------------| USB |------------+
+//                          |            +-----+            |
+//                     B5   | [ ]D13/SCK        MISO/D12[ ] |   B4
+//                          | [ ]3.3V           MOSI/D11[+]~|   B3     Muligame 10-pin header
+//                          | [ ]V.ref     ___    SS/D10[+]~|   B2         +------+
+//                     C0   | [ ]A0       / N \       D9[+]~|   B1---------|10    |
+//                     C1   | [ ]A1      /  A  \      D8[+] |   B0---------|9     |
+//                     C2   | [ ]A2      \  N  /      D7[+] |   D7---------|8     |
+//                     C3   | [ ]A3       \_0_/       D6[+]~|   D6---------|7     |
+//       EEPROM_CLEAR1 -----| [*]A4/SDA               D5[+]~|   D5---------|6     |
+//       EERPOM_CLEAR2 -----| [*]A5/SCL               D4[+] |   D4---------|5     |
+//                          | [ ]A6              INT1/D3[+]~|   D3---------|4     |
+//       pot-wiper ---------| [+]A7              INT0/D2[+] |   D2---------|3     |
+//       pot-HI -----+------| [+]5V                  GND[+] |     ---------|2     |
+//                   +------| [+]RST                 RST[+] |   C6---------|1     |
+//       pot-LO ------------| [+]GND   5V MOSI GND   TX1[ ] |   D0         +------+
+//                          | [ ]Vin   [ ] [ ] [ ]   RX1[ ] |   D1
+//                          |          [ ] [ ] [ ]          |
+//                          |          MISO SCK RST         |
+//                          | NANO-V3                       |
+//                          +-------------------------------+
+//
+//    Notes:
+//      1)  '+': Pin is connected
+//      2)  '*': Short these pins to reset the EEPROM potentiometer calibration
+//      3)  Note, RST and POT-HI are shorted together in the potentiometer cable
+//
+//
+// Arduino Mini Pro pinout:
+//
+//                                                 D0   D1   RST
+//                                  GND  GND  VCC  RX   TX   /DTR
+//                               +--------------------------------+
+//                               |  [ ]  [ ]  [ ]  [ ]  [ ]  [ ]  |
+//                               |              FTDI              |
+// Muligame 10-pin header    D1  | [ ]1/TX                 RAW[ ] |
+//        +------+           D0  | [ ]0/RX                 GND[+] |----------pot-LO
+//        |     1|-----------    | [+]RST        SCL/A5[ ] RST[+] |-----+
+//        |     2|-----------    | [+]GND        SDA/A4[ ] VCC[+] |-----+----pot-HI
+//        |     3|-----------D2  | [+]2/INT0    ___         A3[+] |----------pot-wiper
+//        |     4|-----------D3  |~[+]3/INT1   /   \        A2[ ] |   C2
+//        |     5|-----------D4  | [+]4       /PRO  \       A1[*] |----------EEPROM_CLEAR2
+//        |     6|-----------D5  |~[+]5       \ MINI/       A0[*] |----------EEPROM_CLEAR1
+//        |     7|-----------D6  |~[+]6        \___/    SCK/13[ ] |   B5
+//        |     8|-----------D7  | [+]7          A7[ ] MISO/12[ ] |   B4
+//        |     9|-----------B0  | [+]8          A6[ ] MOSI/11[ ]~|   B3
+//        |    10|-----------B1  |~[+]9                  SS/10[ ]~|   B2
+//        +------+               |           [RST-BTN]            |
+//                               +--------------------------------+
+//
+//    Notes:
+//      1)  '+': Pin is connected
+//      2)  '*': Short these pins to reset the EEPROM potentiometer calibration
+//      3)  Note, RST and POT-HI are shorted together in the potentiometer cable
+//
+//
 #include <stdint.h>
 #include <EEPROM.h>
 
